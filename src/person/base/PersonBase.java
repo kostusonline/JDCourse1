b3f7583fd460bb3ce66529f95c262b758b344f0b
Konstantin Terskikh <kostus.online@gmail.com>
2024-10-28 16:11:29 +0300

    PersonBase: equals, hashCode

--- src/person/base/PersonBase.java
+++ src/person/base/PersonBase.java
@@ -13,6 +13,7 @@ import verifiable.Verifiable;
 import java.time.DateTimeException;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.Objects;
 
 public final class PersonBase implements Person {
 
@@ -31,6 +32,7 @@ public final class PersonBase implements Person {
             throw new ExceptionVerify("Ошибка установки фамилии");
         }
         this.lastName = lastName;
+        updateHash();
     }
 
     private String firstName;
@@ -44,6 +46,7 @@ public final class PersonBase implements Person {
             throw new ExceptionVerify("Ошибка установки имени");
         }
         this.firstName = firstName;
+        updateHash();
     }
 
     private String middleName;
@@ -57,6 +60,7 @@ public final class PersonBase implements Person {
             throw new ExceptionVerify("Ошибка установки отчества");
         }
         this.middleName = middleName;
+        updateHash();
     }
 
     // Дата рождения.
@@ -84,6 +88,7 @@ public final class PersonBase implements Person {
         birthYear = year;
         birthMonth = month;
         birthDay = day;
+        updateHash();
     }
 
     // Пол.
@@ -96,6 +101,51 @@ public final class PersonBase implements Person {
 
     public void setGender(Gender gender) {
         this.gender = gender;
+        updateHash();
+    }
+
+    // Переопределим equals и hashCode
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+        PersonBase that = (PersonBase) o;
+        // Сначала самые быстрые сравнения, потом строки.
+        return this.birthYear == that.birthYear &&
+                this.birthMonth == that.birthMonth &&
+                this.birthDay == that.birthDay &&
+                this.gender == that.gender &&
+                this.lastName.equals(that.lastName) &&
+                this.firstName.equals(that.firstName) &&
+                this.middleName.equals(that.middleName);
+    }
+
+    @Override
+    public int hashCode() {
+        return hash;
+    }
+
+    private int hash;
+    private boolean freezeUpdateHash;
+
+    private void updateHash() {
+        if (freezeUpdateHash) {
+            return;
+        }
+        hash = Objects.hash(
+                this.birthYear,
+                this.birthMonth,
+                this.birthDay,
+                this.gender,
+                this.lastName,
+                this.firstName,
+                this.middleName);
     }
 
     // Инициализация
@@ -103,6 +153,8 @@ public final class PersonBase implements Person {
     public PersonBase(Verifiable<String> nameVerifier, String lastName, String firstName, String middleName,
                       int birthYear, int birthMonth, int birthDay,
                       Gender gender) throws ExceptionVerify, DateTimeException {
+        freezeUpdateHash = true;
+
         this.nameVerifier = nameVerifier;
         setLastName(lastName);
         setFirstName(firstName);
@@ -110,5 +162,8 @@ public final class PersonBase implements Person {
 
         setBirthDate(birthYear, birthMonth, birthDay);
         setGender(gender);
+
+        freezeUpdateHash = false;
+        updateHash();
     }
 }
